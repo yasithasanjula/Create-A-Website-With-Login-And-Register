6817149332b961cb1f59826187d2141b7691413c
Yasitha Sanjula Herath <yasithasanjula2018@gmail.com>
2023-11-16 19:14:47 +0530

    111

--- Login-Page/src/Login.jsx
+++ Login-Page/src/Login.jsx
@@ -4,17 +4,17 @@ import { Link } from 'react-router-dom'
 const Login = () => {
   return (
     <div>
-        <div>
-            <h1>Login</h1>
+        <div className='bg-slate-800 border-slate-400 rounded-md p-8 shadow-lg backdrop-filter backdrop-blur-sm bg-opacity-30 relative '>
+            <h1 className='text-4xl text-whitefont-bold text-center mb-6'>Login</h1>
                 <form action="">
-                    <div>
-                    <input type="email" />
-                    <label htmlFor="">Your Email</label>
+                    <div className='relative my-4'>
+                    <input type="email" className='block w-72 py-2.3 px-0 text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600 peer' placeholder=''/>
+                    <label htmlFor="" className='absolute text-sm text-white duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer:placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'>Your Email</label>
                     </div>
 
-                    <div>
-                    <input type="email" />
-                    <label htmlFor="">Your Email</label>
+                    <div className='relative my-4'>
+                    <input type="password" className='block w-72 py-2.3 px-0 text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:text-white focus:border-blue-600 peer' placeholder=''/>
+                    <label htmlFor="" className='absolute text-sm text-white duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer:placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'>Your Password</label>
                     </div>
 
             <div>
