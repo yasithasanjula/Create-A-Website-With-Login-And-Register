ee0e1e2b189122e87982fa9081469ba5ae9474e2
Yasitha Sanjula Herath <yasithasanjula2018@gmail.com>
2023-11-16 15:55:35 +0530

    111

---
+++ Login-Page/src/App.jsx
@@ -0,0 +1,21 @@
+import react from 'react'
+
+import bg from './assets/bg.jpg';
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+    <div className='text-white h-[100vh] flex justify-center items-center bg-cover' style={{'background' : 'url../src/assets/bg.jpg'}}>
+
+    </div>
+      
+        
+    </>
+  )
+}
+
+export default App
